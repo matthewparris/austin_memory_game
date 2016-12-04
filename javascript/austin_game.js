9803488932f933396480f5c4c44546f0a8f6df85
voodoo15 <arnold.redoblado@gmail.com>
2016-12-03 21:51:38 -0500

    Changed name of id's on card div and found a shuffle function to randomize the array that will house the images

--- javascript/austin_game.js
+++ javascript/austin_game.js
@@ -1,4 +1,4 @@
-var p1Score, p2Score, cardPosition = [];
+var p1Score, p2Score, images = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12];
 
 function updateScore() {
   // Update score on screen
@@ -18,6 +18,29 @@ function p2Scores() {
   updateScore();
 }
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// function getRandomInt(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
 // function addTile(name, image) {
 //   return () => {
 //     "<div class='col-sm-6 col-md-3'><div class='thumbnail' id='" + name + "'><div class='caption'>Austin's Memory Game</div></div></div>"
@@ -26,12 +49,16 @@ function p2Scores() {
 
 function startGame() {
 
-  // Initialze score
+    // Initialze score
     p1Score = 0;
     p2Score = 0;
 
+
     //  Place scores on screen
     updateScore();
+
+    // Card Position reset
+    shuffle(images);
 
     // // Add ID's for the cards
     // $('.thumbnail').each((e) => {
